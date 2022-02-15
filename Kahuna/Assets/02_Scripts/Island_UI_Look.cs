800983950494a1cf0176d52bc911815f54f48d60
MakerJun46 <guswns6711@naver.com>
2022-02-16 01:05:56 +0900

    220216_배경 완성, 카드 제작

---
+++ Kahuna/Assets/02_Scripts/Island_UI_Look.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Island_UI_Look : MonoBehaviour
+{
+    void Update()
+    {
+        CameraLook();
+    }
+
+    public void CameraLook()
+    {
+        this.gameObject.transform.LookAt(Camera.main.transform);
+    }
+}
